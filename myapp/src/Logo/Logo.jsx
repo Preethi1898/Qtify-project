0383377dc9b0a39475c3926b8955aa9e8db7b11f
Preethi Arun <preethisarun1214@gmail.com>
2024-07-29 18:01:55 +0530

    demo

---
+++ myapp/src/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const  Logo =() =>{
+  return <img src={require("../assets/Group 3748.png")} alt="logo" width={67} />;
+}
+ 
+export default Logo;
---
+++ myapp/src/Search/Search.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from "./Search.module.css"
+import {ReactComponent as SearchIcon} from "../assets/Search icon (1).svg";
+
+  const Search = () => {
+  return (
+    <div >
+      <form className={styles.wrapper}  >
+        <input className={styles.search} placeholder="Search a album of your choice" />
+        <div>
+            <button className={styles.searchButton} type='submit' >
+              <SearchIcon/>
+            </button>
+        </div>
+      </form>
+        </div>
+  )
+}
+export default Search;
